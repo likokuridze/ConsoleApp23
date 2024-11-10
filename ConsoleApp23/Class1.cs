0a86a408d2a8b46b9a5ad81c6d8c7116031f247b
LikoKuridze <lkuridze@credo.ge>
2024-11-11 01:32:25 +0400

    Add project files.

---
+++ ConsoleApp23/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IAnimal[] animals = new IAnimal[]
+            {
+                new Dog(),
+                new Cat(),
+                new Cow()
+            };
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine($"{animal.GetType().Name} says: {animal.MakeSound()}");
+            }
+        }
+    }
+}
---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace ConsoleApp23
+{
+    public class Dog : Animal
+    {
+        public override string MakeSound()
+        {
+            return "Woof";
+        }
+    }
+}
+namespace ConsoleApp23
+{
+    public class Cat : Animal
+    {
+        public override string MakeSound()
+        {
+            return "Meow";
+        }
+    }
+}
+namespace ConsoleApp23
+{
+    public class Cow : Animal
+    {
+        public override string MakeSound()
+        {
+            return "Moo";
+        }
+    }
+}
